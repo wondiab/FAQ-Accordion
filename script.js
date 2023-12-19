3de0f9ae8fe3cf07fee09b6871e39bebbfdb5273
wondiab <wondiradabebe@gmail.com>
2023-12-19 12:24:43 +0300

    cleaner-version

--- script.js
+++ script.js
@@ -17,20 +17,3 @@ questions.forEach((question) => {
     }
   });
 });
-// The following code isn't prefered because once the answer is displayed it doesn't collapse
-// questions.forEach((question) => {
-//   question.addEventListener("click", (event) => {
-//     const answer = event.target.nextElementSibling;
-
-//     answer.style.display = "block";
-//     const icon = event.target.querySelector(".icon");
-
-//     if (answer.style.display === "none") {
-//       // If the answer is hidden, set the icon to a plus
-//       icon.innerHTML = `<i class="fa-solid fa-circle-plus"></i>`;
-//     } else {
-//       // If the answer is visible, set the icon to a minus
-//       icon.innerHTML = `<i class="fa-solid fa-circle-minus"></i>`;
-//     }
-//   });
-// });
